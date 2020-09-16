e854e15d253206517c700572cc90a16d7d29d91c
jhmoreira <joaomoreirati98@gmail.com>
2020-09-16 13:36:49 -0300

    Entities,Repositories

---
+++ backend/src/main/java/com/example/demo/entities/Game.java
@@ -0,0 +1,111 @@
+package com.example.demo.entities;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.example.demo.entities.enums.Plataforma;
+
+
+@Entity 
+@Table(name="tb_game")
+public class Game implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String title;
+	private Plataforma platform;
+	
+	@ManyToOne
+	@JoinColumn(name="genre_id")
+	private Genero genre;
+	
+	@OneToMany(mappedBy="game")
+	private List< Record> records= new ArrayList<>();
+	
+	public  Game () {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Plataforma getPlataform() {
+		return platform;
+	}
+
+	public void setPlataform(Plataforma plataforma) {
+		this.platform = plataforma;
+	}
+
+	public Genero getGenero() {
+		return genre;
+	}
+
+	public void setGenero(Genero genero) {
+		this.genre = genero;
+	}
+
+	public List<Record> getRecord() {
+		return records;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((genre == null) ? 0 : genre.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((platform == null) ? 0 : platform.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Game other = (Game) obj;
+		
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		
+		return true;
+	}
+	
+	
+}
---
+++ backend/src/main/java/com/example/demo/entities/Record.java
@@ -0,0 +1,84 @@
+package com.example.demo.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+
+@Entity 
+@Table(name="tb_record")
+public class Record implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private Integer age;
+	private Instant moment;
+	
+	@ManyToOne
+	@JoinColumn(name= "game_id")
+	private Game game;
+
+	public Record() {
+		
+	}
+
+	public Record(Long id, String nome, Integer idade, Instant momento, Game game) {
+		super();
+		this.id = id;
+		this.name = nome;
+		this.age = idade;
+		this.moment = momento;
+		this.game = game;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((game == null) ? 0 : game.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((age == null) ? 0 : age.hashCode());
+		result = prime * result + ((moment == null) ? 0 : moment.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Record other = (Record) obj;
+		
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+	
+		return true;
+	}
+	
+	
+}
+
+
+
+
