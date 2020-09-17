ca84dec31152a9eb05abc830e1aebbf8a9798ae0
jhmoreira <joaomoreirati98@gmail.com>
2020-09-17 01:28:18 -0300

    GET Records

--- backend/src/main/java/com/example/demo/services/RecordService.java
+++ backend/src/main/java/com/example/demo/services/RecordService.java
@@ -3,6 +3,8 @@ package com.example.demo.services;
 import java.time.Instant;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -38,6 +40,11 @@ Record entity= new Record();
 		
 		return new RecordDTO(entity);
 		
+	}
+	@Transactional(readOnly=true)
+	public Page<RecordDTO> findByMoments(Instant minDate, Instant maxDate, PageRequest pageRequest) {
+		// TODO Auto-generated method stub
+		return repository.findByMoments(minDate,maxDate, pageRequest).map(x-> new RecordDTO(x));
 	}
 		
 }
